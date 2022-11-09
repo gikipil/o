14ca4f7e7dc43e7d162d9ceda35d8b52888428aa
gikipil <frostcool1998@gmail.com>
2022-11-09 10:59:37 +0700

    update .gitignore

--- Task_1_1_1/src/test/java/HeapsortTest.java
+++ Task_1_1_1/src/test/java/HeapsortTest.java
@@ -10,24 +10,11 @@ import org.junit.jupiter.api.Test;
 public class HeapsortTest {
 
     /**
-     * Тестирование обычного списка в неправильном порядке.
-     */
-
-    @Test
-    void sort1() {
-        int[] arr = {1, 5, 3, 9, 12, 6, 33};
-        int[] ans = {1, 3, 5, 6, 9, 12, 33};
-        System.out.println("Создан массив: 1,5,3,9,12,6,33");
-        assertArrayEquals(Heapsort.sort(arr), ans);
-        System.out.println("Массив успешно отсортирован!");
-    }
-
-    /**
      * Тестирование списка с повторяющимися элементами.
      */
 
     @Test
-    void sort2() {
+    void repeatedElementsArrayTest() {
         int[] arr = {56, 3, 4, 3, 1, 3};
         int[] ans = {1, 3, 3, 3, 4, 56};
         System.out.println("Создан массив: 56,3,4,3,1,3");
@@ -40,7 +27,7 @@ public class HeapsortTest {
      */
 
     @Test
-    void sort3() {
+    void emptyArrayTest() {
         int[] arr = {};
         int[] ans = {};
         System.out.println("Создан пустой массив");
@@ -53,7 +40,7 @@ public class HeapsortTest {
      */
 
     @Test
-    void sort4() {
+    void sortedArrayTest() {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         int[] ans = {1, 2, 3, 4, 5, 6, 7, 8};
         System.out.println("Создан массив: 1,2,3,4,5,6,7,8");
@@ -66,7 +53,7 @@ public class HeapsortTest {
      */
 
     @Test
-    void sort5() {
+    void negativeArrayTest() {
         int [] arr = {0, -1, -2, -3, -4, -5};
         int [] ans = {-5, -4, -3, -2, -1, 0};
         System.out.println("Создан массив: 0,-1,-2,-3,-4,-5");
