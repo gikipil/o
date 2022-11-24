43d7d0d5cc16c7b849fded0ed2b574675a8d725c
gikipil <frostcool1998@gmail.com>
2022-11-25 00:22:01 +0700

    first

---
+++ Task_1_2_1/src/main/java/org/example/Stack.java
@@ -0,0 +1,119 @@
+package org.example;
+
+/**
+ * A class implementing the stack
+ */
+
+public class Stack {
+
+    /**
+     * An array that stores objects
+     */
+
+    private Object [] list;
+
+    /**
+     * Stack size
+     */
+
+    private int size;
+
+    /**
+     * The actual size of the array
+     */
+
+    private int cap;
+
+    /**
+     * Constructor that sets initial values
+     */
+
+    public Stack(){
+        list = new Object[10];
+        size = 0;
+        cap = 10;
+    }
+
+    /**
+     *Method that increases the size of the array
+     *if there is not enough space
+     */
+
+    private void realloc () {
+        cap *= 2;
+        Object [] temp = new Object [size];
+        System.arraycopy(list, 0, temp, 0, size);
+        list = new Object [cap];
+        if (size >= 0) {
+            System.arraycopy(temp, 0, list, 0, size);
+        }
+    }
+
+    /**
+     *The method adds an object to the stack
+     * @param obj The object to add to the stack
+     */
+    public void push (Object obj) {
+        if (size == cap){
+            realloc ();
+        }
+        list[size] = obj;
+        size ++;
+    }
+
+    /**
+     *The method adds a group of objects to the stack
+     * @param obj Array of objects to be added to the stack
+     */
+
+    public void pushStack (Object [] obj) {
+        for(Object i: obj) {
+            push(i);
+        }
+    }
+
+    /**
+     * The method removes 1 object from the stack
+     */
+
+    public void pop () {
+        if(size > 0){
+            size --;
+            list[size] = null;
+        }
+    }
+
+    /**
+     * The method removes the specified number of objects from the stack
+     * @param k number of objects
+     */
+
+    public void popStack (int k) {
+        for(int i = 0; i < k; i++){
+            pop();
+        }
+    }
+
+    /**
+     * Method for determining the stack size
+     * @return stack size
+     */
+
+    public int count () {
+        return size;
+    }
+
+    /**
+     * The method that returns the stack
+     * @return stack
+     */
+
+    public Object [] show (){
+        if (size == 0){
+            return new Object[0];
+        }
+        Object [] ans = new Object[size];
+        System.arraycopy(list,0,ans,0,size);
+        return ans;
+    }
+}
---
+++ Task_1_2_1/src/test/java/StackTest.java
@@ -0,0 +1,100 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Random;
+import org.example.Stack;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Testing class
+ */
+
+public class StackTest {
+
+    /**
+     * Testing basic functions
+     */
+
+    @Test
+    void functionTest(){
+        Stack stack = new Stack();
+        stack.push(2);
+        stack.push(4);
+        stack.push(7);
+        stack.push(1);
+        stack.push(4);
+        stack.push(2);
+        stack.pushStack(new Integer []{4, 6, 3, 1, 98});
+        stack.pushStack(new Integer []{323, 11, 123, 444});
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.popStack(1);
+        stack.popStack(3);
+        assertArrayEquals(stack.show(), new Integer[]{2, 4, 7, 1, 4, 2, 4, 6});
+        System.out.println("Successfully completed");
+    }
+
+    /**
+     * Testing an empty stack
+     */
+
+    @Test
+    void emptyTest(){
+        Stack stack = new Stack();
+        stack.pop();
+        stack.pop();
+        stack.push(3);
+        stack.pop();
+        assertArrayEquals(stack.show(), new Object[0]);
+        System.out.println("Successfully completed");
+    }
+
+    /**
+     * Testing multiple stacks
+     */
+
+    @Test
+    void multipleTest () {
+        Stack stack1 = new Stack();
+        Stack stack2 = new Stack();
+        Stack stack3 = new Stack();
+        stack1.push(1);
+        stack1.push(67);
+        stack2.push("dsa");
+        stack3.push(true);
+        stack2.push("tea");
+        stack1.push(54);
+        stack3.push(false);
+        stack1.push(3);
+        stack2.push("car");
+        stack2.push("bed");
+        stack3.push(false);
+        assertArrayEquals(stack1.show(), new Integer [] {1, 67, 54, 3});
+        assertArrayEquals(stack2.show(), new String [] {"dsa", "tea", "car", "bed"});
+        assertArrayEquals(stack3.show(), new Boolean[] {true, false, false});
+        System.out.println("Successfully completed");
+    }
+    /**
+     * Testing random stacks
+     */
+
+    @Test
+    void randomTest (){
+        Random generator = new Random();
+        int len = generator.nextInt(1000);
+        Stack stack = new Stack();
+        Stack ans = new Stack();
+        for(int i = 0; i < len; i++){
+            Integer val = generator.nextInt();
+            stack.push(val);
+            ans.push(val);
+        }
+        len = generator.nextInt(1000);
+        for(int i = 0; i < len; i++){
+            stack.pop();
+            ans.pop();
+        }
+        assertArrayEquals(stack.show(), ans.show());
+        System.out.println("Successfully completed");
+    }
+}
