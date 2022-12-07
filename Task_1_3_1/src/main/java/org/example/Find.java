b584de85baccac96ce8ebf81783ab48253d33226
gikipil <frostcool1998@gmail.com>
2022-12-07 19:29:06 +0700

    changhe standartcharset

--- Task_1_3_1/src/main/java/org/example/Find.java
+++ Task_1_3_1/src/main/java/org/example/Find.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 
 /**
@@ -31,7 +32,7 @@ public class Find {
 
     public String openfile (String name) throws IOException {
         String file = "src/test/resources/" + name;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
         String line = "";
         String text = "";
         while(line != null){
